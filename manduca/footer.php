506f39a6fe1ae284794ff4d2812f65502c2cc0de
Zsolt Edelenyi <edelenyi.zsolt@akadalymentes-honlap.hu>
2015-12-04 13:53:43 +0100

    Version 2.0.5
    Approved by the WordPress theme reviewer.

---
+++ manduca/footer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Manduca
+ *
+ * @since 1.0 */
+ 
+?>
+		</div><!-- #main .wrapper -->
+		<div id="footer-wrapper" class="footer-wrapper">
+			<footer id="colophon" role="contentinfo" itemtype="https://schema.org/WPFooter" itemscope="itemscope" >
+				<div class="site-info">
+					<p>&copy; <?php echo date( 'Y' ) ."., " .esc_html( get_option ( 'copyright_text' ) ); ?></p>
+								
+					
+				</a>
+				</div><!-- .site-info -->
+				
+			</footer><!-- #colophon -->
+		</div>
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
