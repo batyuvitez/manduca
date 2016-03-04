5f58816a8243726b3ac212a3a6d3c1ef077c1f56
Zsolt Edelényi <batyuvitez@gmail.com>
2016-03-04 15:59:58 +0100

    commit #16.2.4
    Clearfix deleted from header

--- manduca/header.php
+++ manduca/header.php
@@ -39,8 +39,6 @@
 					<?php get_search_form(); ?>
 			</header>
 		</div>
-			
-		<div class="vonalzo"></div>
 	
 			<?php if( ( is_home() || is_front_page() ) && get_header_image() ) : ?>
 				<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php header_image(); ?>" class="header-image" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( manduca_get_header_image_alt() ); ?>" /></a>
