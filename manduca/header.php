300f3af8c46dcef5fe2f0ecb889a04077bc0824e
oaron <oaron1@gmail.com>
2016-03-21 23:05:14 +0100

    banner landmark was duplicate, screen readers announce twice.

--- manduca/header.php
+++ manduca/header.php
@@ -30,7 +30,7 @@
 <body id="total" <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 	
-	<div id="masthead" class="site-header" role="banner"  itemtype="https://schema.org/WPHeader" itemscope="itemscope">
+	<div id="masthead" class="site-header"  itemtype="https://schema.org/WPHeader" itemscope="itemscope">
 		<div id="header-bar" class="header-bar">
 			<header>
 				<a class="screen-reader-text skip-link" href="#content"><?php _e( 'Skip to content', 'manduca' ); ?></a>
