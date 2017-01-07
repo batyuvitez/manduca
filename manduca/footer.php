02eb050bf7f2245f95b1206db7fcb885a14f8010
Zsolt Edelényi <batyuvitez@gmail.com>
2017-01-07 06:45:04 +0100

    #17.1.2: footer design changes
    on line per menu item on mobil
    - button-like in footer

--- manduca/footer.php
+++ manduca/footer.php
@@ -36,7 +36,7 @@
 							 * @since 16.12
 							 * */
 							
-							echo apply_filters( 'manduca_copyright_text' , sprintf ('<p>&copy; %1$s, %2$s</p>', date( 'Y' ), get_bloginfo() ) ) ;
+							echo apply_filters( 'manduca_copyright_text' , sprintf ('<p class="copyright-text">&copy; %1$s, %2$s</p>', date( 'Y' ), get_bloginfo() ) ) ;
 							?>
 								
 						</div><!-- .site-info -->
