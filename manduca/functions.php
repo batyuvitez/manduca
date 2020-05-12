7958e2f955eabef5de6643578275ccca0aa6598f
batyuvitez <user@web25.hu>
2020-05-12 14:40:18 +0200

    caption to featured image

--- manduca/functions.php
+++ manduca/functions.php
@@ -7,7 +7,7 @@
 
 /*  This file is part of WordPress theme named Manduca - focus on accessibility.
  *
-	Copyright (C) 2015-2019  Zsolt Edelényi (ezs@web25.hu)
+	Copyright (C) 2015-2020  Zsolt Edelényi (ezs@web25.hu)
 
     Source code is available at https://github.com/batyuvitez/manduca
     Manduca is free software: you can redistribute it and/or modify
--- manduca/template-parts/posts/content-excerpt.php
+++ manduca/template-parts/posts/content-excerpt.php
@@ -1,13 +1,22 @@
 <?php
-/**
- * Displaying excerpts of search
- * IMPORTANT! Includes the hook
+/*  This file is part of WordPress theme named Manduca - focus on accessibility.
  *
- * This files is the same as excerpt.
- * 
- * @ Theme Manduca - focus on accessibility
- * @ Since 18.1.6
- **/
+	Copyright (C) 2015-2020  Zsolt Edelényi (ezs@web25.hu)
+
+    Source code is available at https://github.com/batyuvitez/manduca
+    Manduca is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    in /assets/docs/licence.txt.  If not, see <https://www.gnu.org/licenses/>.
+*/
  
 ?>
 
@@ -31,7 +40,7 @@
 		
 		
 			<?php if ( has_post_thumbnail() ) : ?>
-				 <div class="post-thumbnail">
+				 <div class="excerpt-thumbnail">
 					<?php the_post_thumbnail( 'excerpt-size' ); // defined in Manduca 268*178 | aspect-ratio: 1.5:1  ?>
 				 </div>
 			<?php endif; ?>
