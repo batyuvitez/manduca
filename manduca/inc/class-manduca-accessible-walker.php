729e0c9c8793a9aeb0977d1e87e938aa38597d13
batyuvitez <user@web25.hu>
2021-05-13 15:42:21 +0200

    480:aria-expanded was swapped

--- manduca/inc/class-manduca-accessible-walker.php
+++ manduca/inc/class-manduca-accessible-walker.php
@@ -118,7 +118,9 @@ class Manduca_accessible_walker extends Walker_Nav_Menu  {
 			$item_output .= '<button class="dropdown-toggle level-'.$depth.'" ';			
 			$item_output .= 'aria-haspopup="true" ';
 			$item_output .= 'aria-controls="'. $this->aria_controls.'" ';
-			$item_output .= 'aria-labelledby="'.$this->aria_labelledby .'">';
+			$item_output .= 'aria-labelledby="'.$this->aria_labelledby .'" ';
+			$item_output .= 'aria-expanded="false"';
+			$item_output .= '>';
 			$item_output .= manduca_get_svg( array( 'icon' => 'caret-down'));
 			$item_output .= manduca_get_svg( array( 'icon' => 'close'));
 			$item_output .='</button>';
