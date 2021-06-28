8b770cd9bbf2bd0e0f5aa5fa2b2c7776b18349a9
batyuvitez <user@web25.hu>
2021-06-28 12:02:40 +0200

    493: Them check:  must not use <title> tags.

--- manduca/inc/class-svg-functions.php
+++ manduca/inc/class-svg-functions.php
@@ -39,8 +39,6 @@ class SVG_Functions {
 			 *     Parameters needed to display an SVG.
 			 *
 			 *     @type string $icon  Required SVG icon filename.
-			 *     @type string $title Optional SVG title.
-			 *     @type string $desc  Optional SVG description.
 			 * }
 			 * @return string SVG markup.
 			 */
@@ -62,8 +60,6 @@ class SVG_Functions {
 			  // Set defaults.
 			  $defaults = array(
 				  'icon'        => '',
-				  'title'       => '',
-				  'desc'        => '',
 				  'fallback'    => false,
 			  );
 		  
@@ -79,47 +75,8 @@ class SVG_Functions {
 			  // Set ARIA.
 			  $aria_labelledby = '';
 		  
-			  /**
-			   * Manduca doesn't use the SVG title or description attributes; non-decorative icons are described with .screen-reader-text.
-			   *
-			   * However, child themes can use the title and description to add information to non-decorative SVG icons to improve accessibility.
-			   * Example 1 with title:
-			   * 			<?php echo manduca_get_svg( array( 'icon' => 'arrow-right', 'title' => __( 'This is the title', 'textdomain' ) ) ); ?>
-			   
-			   * Example 2 with title and description:
-			   *			<?php echo manduca_get_svg( array( 'icon' => 'arrow-right', 'title' => __( 'This is the title', 'textdomain' ), 'desc' => __( 'This is the description', 'textdomain' ) ) ); ?>
-			   
-			   * See https://www.paciellogroup.com/blog/2013/12/using-aria-enhance-svg-accessibility/.
-			   **/
-			  if ( $this->svg_name['title'] ) {
-				  $aria_hidden     = '';
-				  $unique_id       = uniqid();
-				  $aria_labelledby = ' aria-labelledby="title-' . $unique_id . '"';
-		  
-				  if ( $this->svg_name['desc'] ) {
-					  $aria_labelledby = ' aria-labelledby="title-' . $unique_id . ' desc-' . $unique_id . '"';
-				  }
-			  }
-		  
-				
-			  // Begin SVG markup.
-			 
-			  // Display the title.
-			  if ( $this->svg_name['title'] ) {
-								$title = '<title id="title-' . $unique_id . '">' . esc_html( $this->svg_name['title'] ) . '</title>';
-						
-								// Display the desc only if the title is already set.
-								if ( $this->svg_name['desc'] ) {
-									$description = '<desc id="desc-' . $unique_id . '">' . esc_html( $this->svg_name['desc'] ) . '</desc>';
-								}
-								else {
-										$description = '';
-								}
-			  }
-			  else {
-				  $title= '';
-			  }
-		  
+			  
+			  
 		  
 			  /*
 			   * Display the icon.
