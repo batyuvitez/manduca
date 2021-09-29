7336759e45c597aa287b8f57dee5ef524370335b
batyuvitez <batyuvitez@gmail.com>
2021-09-29 10:11:24 +0200

    532: format tag and category archive pages

--- manduca/category.php
+++ manduca/category.php
@@ -30,8 +30,8 @@
 <?php get_header() ?>
 
 	<?php if ( have_posts() ) : ?>
-		<header class='archive-header'>
-			<h1><?php echo single_cat_title( '', false ); ?></h1>
+		<header>
+			<h1 class="taxonomy-header"><?php echo single_cat_title( '', false ); ?></h1>
 
 		<?php 
 			$term_description = category_description();
--- manduca/tag.php
+++ manduca/tag.php
@@ -26,7 +26,7 @@
 
 		<?php if ( have_posts() ) : ?>
 			<header>
-				<h1 tabindex="0"><?php echo single_cat_title( '', false ) ; ?></h1>
+				<h1 class="taxonomy-header"><?php echo single_cat_title( '', false ) ; ?></h1>
 
 			<?php
 					// Show an optional term description.
